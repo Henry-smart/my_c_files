cd897a9ecee542fae94c2c66f15c80291dcb861a
henrysmart <henryeziechina2@gmail.com>
2022-10-25 23:02:29 +0100

    updated cal.c source code

--- c_beginer_code_files/cal.c
+++ c_beginer_code_files/cal.c
@@ -6,23 +6,17 @@
  */
 int main(void)
 {
-	int i;
 
 	printf("\n\n===*HsmartBot*===");
 
 	printf("\n\nHello!! >>HsmartBot will do basic calculations!<<\n");
 
-	printf("\nTo calculate press(1 + enter) or (0 + enter)to quit:");
-
-	scanf("%d", &i);
-
-	if(i==1)
-		
-	{	
-
 	float a;
 	float b;
 	int dec;
+	int i;
+
+	do{
 
 	printf("\ninput first num and press enter:");
 
@@ -36,40 +30,35 @@ int main(void)
 
 	scanf("%d", &dec);
 
-	if(dec==1)
+	if (dec == 1)
 	{
 		printf("\nThe sum of your input [%g + %g] is =%3g\n\n", a, b, a + b);
-		main();
 	}
-	else if(dec==2)
+	else if (dec == 2)
 	{
 		printf("\nThe difference of your input [%g - %g] is =%3g\n\n", a, b, a - b);
-		main();
 	}
-	else if(dec==3)
+	else if (dec == 3)
 	{
 		printf("\nThe Quotient of your input [%g / %g] is =%3g\n\n", a, b, a / b);
-		main();
 	}
-	else if(dec==4)
+	else if (dec == 4)
 	{
 		printf("\nThe product of your input [%g x %g] is =%3g\n\n", a, b, a * b);
-		main();
 	}
 	else
 	{
 		printf("\n\nERROR!! >>No matching cal-type selected! --> **please select either (1),(2),(3) or (4)**\n\n");
-		main();
 	}
-	}
-	else if(i==0)
-	{
-	printf("\n\nYOU QUITED\n\n===*HsmartBot*===\n\n"); 
-	}
-	else
-	{
-	main();
-	}
+
+
+	printf("Do you want to perform another calculation [1: yes / 0: Quit]:");
+
+	scanf("%d", &i);
+
+	}while (i == 1);
+
+	printf("\n\nYOU QUITED\n\n===*HsmartBot*===\n\n");
 
 	return (0);
 
--- c_beginer_code_files/main.h
+++ c_beginer_code_files/main.h
@@ -1,6 +1,6 @@
 // #ifndef _MAIN_H
 // #define _MAIN_H
 
-#include <stdio.h>
+#include <stdio>
 
-// #endif /*_MAIN_H*/
+#endif
