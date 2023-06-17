d311593d8d9130f89364ea361cffbf2271714df0
henrysmart <henryeziechina2@gmail.com>
2023-06-17 10:54:27 +0100

    updated

--- c_beginer_code_files/cal.c
+++ c_beginer_code_files/cal.c
@@ -12,7 +12,8 @@ int main(void)
 	printf("\n\nHello!! >>HSMART-BOT will do your basic calculations!<<\n");
 
 	float a, b;
-	int dec, i;
+	int d; 
+	int i;
 
 	do {
 
@@ -21,29 +22,39 @@ int main(void)
 	printf("\ninput second num and press enter:");
 	scanf("%g", &b);
 	printf("\ncal-type: please select and press enter>> (1) for sum,");
-	printf("(2) for minus, (3) for divde, (4) for multiply:");
-	scanf("%d", &dec);
+	printf(" (2) for minus, (3) for divde, (4) for multiply:");
 
-	if (dec == 1)
+	scanf("%d", &d);
+
+	switch (d)
+	{
+	case 1:
 		printf("\nThe sum of your input [%g + %g] is =%3g\n\n", a, b, a + b);
-	else if (dec == 2)
+		break;
+	case 2:
 		printf("\nThe difference of your input [%g - %g] is =%3g\n\n", a, b, a - b);
-	else if (dec == 3)
+		break;
+	case 3:
 		printf("\nThe Quotient of your input [%g / %g] is =%3g\n\n", a, b, a / b);
-	else if (dec == 4)
+		break;
+	case 4:
 		printf("\nThe product of your input [%g x %g] is =%3g\n\n", a, b, a * b);
-	else
+		break;
+	default:
 	{
 		printf("\n\nERROR!! >>No matching cal-type selected!");
+		
+
 		printf("--> **please select either (1),(2),(3) or (4)**\n\n");
 	}
+	}
 
-	printf("Do you want to perform another calculation [1: yes / 0: Quit]:");
+	printf("Do you want to perform another calculation [1: yes / 2: Quit]:");
 	scanf("%d", &i);
 
 	} while (i == 1);
 
-	printf("\n\nYOU QUITED\n\n===*HSMART-BOT*===\n\n");
+	printf("\n\nYOU QUIT\n\n===*HSMART-BOT*===\n\n");
 
 	return (0);
 }
---
+++ c_beginer_code_files/sum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * add - entry point to the function
+ * int a: first arg
+ * int b: second arg
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	a, b = 5, 6;
+	int sum;
+
+	sum = a + b;
+	printf("The value of sum of a+b is:%d\n", sum);
+
+	return (sum);
+}
+
+
+
